caa3c6993517b653f1fbf82b862f3297bab32496
Alvin Xue <19786654@qq.com>
2016-09-16 13:52:23 +0800

    update doc

--- doc/html/search/functions_0.js
+++ doc/html/search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['buffer',['Buffer',['../classXXYY_1_1CarriageBuffers.html#aadbe469a3047a1fcb6f9ae14d4405b76',1,'XXYY::CarriageBuffers']]],
+  ['buffer',['Buffer',['../classXXYY_1_1CarriageBuffers.html#a40134e635bff04a8d8cbe1ecd0452955',1,'XXYY::CarriageBuffers::Buffer()'],['../classXXYY_1_1CarriageDipatcher.html#aeca012347d8102f747bfb6a2639aa796',1,'XXYY::CarriageDipatcher::Buffer()']]],
   ['buffers',['Buffers',['../classXXYY_1_1CarriageDipatcher.html#a10e95ed5a9606d30e50363971890a866',1,'XXYY::CarriageDipatcher']]],
   ['buffers_5fsize',['buffers_size',['../classXXYY_1_1CarriageDipatcher.html#ad2808daf8db1d1bb4afdda1e7f7f6e10',1,'XXYY::CarriageDipatcher']]]
 ];
